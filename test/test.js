274f03291307e5516b26e0e7bf25d03f2d55e48f
Christopher McKay <mckaycr@gmail.com>
2021-01-25 14:39:41 -0500

    initial run at the power method

--- test/test.js
+++ test/test.js
@@ -1,11 +1,10 @@
 'use strict';
 var expect = require('chai').expect;
 var Grill = require('../index.js');
-
+var myGrill = new Grill('192.168.0.42', 8080);
 describe('grill', function(){
   describe('status()', function(){
     it('it should return an object containing grill data', function(done){
-      var myGrill = new Grill('192.168.0.42', 8080);
         myGrill.status(function(res){
         expect(res).to.have.a.property('temp');
         expect(res).to.have.a.property('tempSet');
@@ -16,8 +15,26 @@ describe('grill', function(){
     });
   });
   describe('power()',function(){
-   it('it should turn the grill on and off',function(done){
-    // need some test cases
+   it('it should turn the grill on',function(done){
+    myGrill.power('on',function(data){
+     expect(res).to.have.a.property('temp');
+     expect(res).to.have.a.property('tempSet');
+     expect(res).to.have.a.property('probe');
+     expect(res).to.have.a.property('state');
+     done();
+    });
+   });
+   it('it should turn the grill off',function(done){
+    myGrill.power('off',function(res){
+     expect(res).to.have.a.property('temp');
+     expect(res).to.have.a.property('tempSet');
+     expect(res).to.have.a.property('probe');
+     expect(res).to.have.a.property('state');
+     done();
+    });
+   })
+   it('it should only accept On or Off as an argument',function(done){
+    expect(() => myGrill.power('test',function(err,data){})).to.throw();
     done();
    });
   });
