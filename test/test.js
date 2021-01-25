97d5d131f26795c69a8ca6c6d087abbf0906d00f
Christopher McKay <mckaycr@gmail.com>
2021-01-25 10:08:02 -0500

    added second probe data

--- test/test.js
+++ test/test.js
@@ -3,15 +3,14 @@ var expect = require('chai').expect;
 var Grill = require('../index.js');
 
 describe('grill', function(){
-  describe('data()', function(){
+  describe('status()', function(){
     it('it should return an object containing grill data', function(done){
       var myGrill = new Grill('127.0.0.1', 8080);
-      myGrill.data(function(res){
+        myGrill.status(function(res){
+        console.log(res);
         expect(res).to.have.a.property('state');
         expect(res).to.have.a.property('temperature');
-        expect(res).to.have.a.property('probe');
-        expect(res).to.have.a.property('desiredTemperature');
-        expect(res).to.have.a.property('desiredProbeTemp');
+        expect(res).to.have.a.property('probes');
         expect(res).to.have.a.property('fireState');
         expect(res).to.have.a.property('warning');
         done();
