d82b25cbbdc7d24b55df8823626d14be9c83db28
Christopher McKay <mckaycr@gmail.com>
2021-01-25 12:07:07 -0500

    adding test case boilerplates to my test script

--- test/test.js
+++ test/test.js
@@ -5,9 +5,8 @@ var Grill = require('../index.js');
 describe('grill', function(){
   describe('status()', function(){
     it('it should return an object containing grill data', function(done){
-      var myGrill = new Grill('127.0.0.1', 8080);
+      var myGrill = new Grill('192.168.0.42', 8080);
         myGrill.status(function(res){
-        console.log(res);
         expect(res).to.have.a.property('temp');
         expect(res).to.have.a.property('tempSet');
         expect(res).to.have.a.property('probe');
@@ -16,4 +15,16 @@ describe('grill', function(){
       });
     });
   });
+  describe('power()',function(){
+   it('it should turn the grill on and off',function(done){
+    // need some test cases
+    done();
+   });
+  });
+  describe('temp()',function(){
+   it('it should set the temperature for the grill and the probes',function(done){
+    // need some test cases
+    done();
+   })
+  })
 });
